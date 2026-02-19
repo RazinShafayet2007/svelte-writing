8a42d29a2c3729873a3246d9c0b0d3efb62d99a6
razinshafayet <sumonislamk29@gmail.com>
2026-02-19 23:22:35 +0600

    wip

---
+++ packages/svelte/src/compiler/index.ts
@@ -0,0 +1,22 @@
+
+import type { LegacyRoot } from './types/legacy-nodes.js';
+import type {
+	CompileOptions,
+	CompileResult,
+	ValidatedCompileOptions,
+	ModuleCompileOptions
+} from '#compiler';
+import type { AST } from './public.js';
+
+import { walk as zimmerframe_walk } from 'zimmerframe';
+import { convert } from './legacy.js';
+import { parse as _parse, Parser } from './phases/1-parse/index.js';
+import { remove_typescript_nodes } from './phases/1-parse/remove_typescript_nodes.js';
+import { parse_stylesheet } from './phases/1-parse/read/style.js';
+import { analyze_component, analyze_module } from './phases/2-analyze/index.js';
+import { transform_component, transform_module } from './phases/3-transform/index.js';
+import { validate_component_options, validate_module_options } from './validate-options.js';
+import * as state from './state.js';
+
+export { default as preprocess } from './preprocess/index.js';
+export { print } from './print/index.js';
